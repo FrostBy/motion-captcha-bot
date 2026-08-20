06c2fa95f0c9f212a0de41db69289ad822bd4869
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-08-20 17:46:56 -0400

    Add Nix flake packaging

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Motion-readable Telegram captcha bot";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          motion-captcha-bot = pkgs.callPackage ./nix/package.nix { };
+
+          default = motion-captcha-bot;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_22
+              pkgs.ffmpeg
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
+    };
+}
